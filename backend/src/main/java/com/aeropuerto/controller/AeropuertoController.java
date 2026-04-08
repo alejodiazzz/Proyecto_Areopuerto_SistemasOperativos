c972e4ac7c95a9dbe1152a2536a0fffe91042da3
Estefania Rojas <yeimy.rojas02@uptc.edu.co>
2026-04-08 18:54:07 -0500

    Actualización del backend y frontend

--- backend/src/main/java/com/aeropuerto/controller/AeropuertoController.java
+++ backend/src/main/java/com/aeropuerto/controller/AeropuertoController.java
@@ -1,13 +1,16 @@
 package com.aeropuerto.controller;
 
-import com.aeropuerto.model.AvionThread;
-import com.aeropuerto.service.AeropuertoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aeropuerto.model.AvionThread;
+import com.aeropuerto.service.AeropuertoService;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class AeropuertoController {
 
     @Autowired
